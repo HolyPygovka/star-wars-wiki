a69ed6d1c3882ce46d2b1027600a525bcc3e4a59
unknown <r2598509@gmail.com>
2020-05-29 22:00:58 +0300

    les11 create all components and start random planet

---
+++ src/components/ItemsList/ItemsList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './ItemsList.css'
+
+const ItemsList = () => {
+    return(
+        <ul className="ItemsList">
+            <li>
+                First preson
+            </li>
+            <li>
+                Second preson
+            </li>
+            <li>
+                Third preson
+            </li>
+        </ul>
+    );
+}
+
+export default ItemsList;
--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -20,4 +20,8 @@ export default class SwapiService {
     getPerson(id) {
         return this.getData(`/people/${id}/`);
     }
+
+    getPlanet(id) {
+        return this.getData(`/planets/${id}/`);
+    }
 }
