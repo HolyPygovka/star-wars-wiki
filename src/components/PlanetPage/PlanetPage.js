5cd6e3ffb306bc372e8b3c33847d3dc5c058fdce
unknown <r2598509@gmail.com>
2020-07-08 11:28:46 +0300

    fix error

--- src/components/PlanetPage/PlanetPage.js
+++ src/components/PlanetPage/PlanetPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemsList from '../ItemsList';
 import DetailsInfo from '../DetailsInfo';
 
-import './PeoplePage.css'
+import './PlanetPage.css'
 import ErrorComponent from '../ErrorComponent';
 import SwapiService from '../../services/SwapiService';
 import Row from '../Row';
